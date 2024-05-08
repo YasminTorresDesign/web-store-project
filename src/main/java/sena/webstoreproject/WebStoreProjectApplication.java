4c58e2e62baeb1c5e22b3d8a1ef64a0f3d7d3281
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-05-08 14:57:17 -0500

    first commit

---
+++ src/main/java/sena/webstoreproject/WebStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package sena.webstoreproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebStoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sena/webstoreproject/WebStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sena.webstoreproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
